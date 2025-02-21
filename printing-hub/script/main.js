00d33ef78de0c0b983780f774afa7a384a3d162c
Omar Nashawati <oceanprime.on@gmail.com>
2025-02-21 18:46:59 +0300

    fix

---
+++ printing-hub/script/main.js
@@ -0,0 +1,12 @@
+console.log("mian");
+
+import './navbar.js';
+import {renderBanners} from './render-banners.js';
+import {renderElemenator} from './render-elemenators.js';
+import {renderServices} from './render-services.js';
+import {renderReviews} from './render-reviews.js';
+
+renderBanners();
+renderElemenator();
+renderServices();
+renderReviews()

70e59169142c0c253d44426dcd8f976d702d39f5
Omar Nashawati <oceanprime.on@gmail.com>
2025-02-21 18:42:08 +0300

    fix

--- printing-hub/main.js
+++ printing-hub/main.js
@@ -1,10 +1,10 @@
 console.log("mian");
 
-import '../script/navbar.js';
-import {renderBanners} from '../script/render-banners.js';
-import {renderElemenator} from '../script/render-elemenators.js';
-import {renderServices} from '../script/render-services.js';
-import {renderReviews} from '../script/render-reviews.js';
+import './script/navbar.js';
+import {renderBanners} from './script/render-banners.js';
+import {renderElemenator} from './script/render-elemenators.js';
+import {renderServices} from './script/render-services.js';
+import {renderReviews} from './script/render-reviews.js';
 
 renderBanners();
 renderElemenator();

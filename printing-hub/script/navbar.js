e835a6ce207cce0dd5bf3da51154a1d20d811ca4
Omar Nashawati <oceanprime.on@gmail.com>
2025-08-02 16:16:23 +0300

    • delete old files • create vite project and clear it • add new logo • main colors

--- printing-hub/script/navbar.js
+++
@@ -1,9 +0,0 @@
-import {cart} from '../data/cart.js'
-
-const cartItems = document.querySelector('.cart-items');
-const cartTotal = document.querySelector('.cart-total');
-
-
-cartItems.innerHTML = cart.length;
-
-
